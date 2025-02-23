d380bfee47394ff7f5e75c4e6b7be03ac6777f60
Raushan132 <raushankumar132abc@gmail.com>
2025-02-23 06:25:55 +0530

    add auth

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import createStore from 'react-auth-kit/createStore';
+import AuthProvider from 'react-auth-kit';
+
+
+const store = createStore({
+  authName:'_auth',
+  authType:'localstorage',
+  cookieDomain: window.location.hostname,
+  cookieSecure: window.location.protocol === 'https:',
+});
 
 createRoot(document.getElementById('root')).render(
-
+  <AuthProvider store={store}>
   <BrowserRouter>
     <StrictMode>
       <App />
     </StrictMode>,
   </BrowserRouter>
+  </AuthProvider>
 
 )
